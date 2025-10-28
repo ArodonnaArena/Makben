51571e487fa2fc3ae5cfb6dbcfaca5a6e4e670e3
Makanjuola Benjamin <cosmicrocketadventure@gmail.com>
2025-10-28 01:09:41 +0100

    chore(next): allow Render uploads as remote images for Next/Image (makben-backend.onrender.com/uploads)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,6 +13,22 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'makben-backend.onrender.com',
+        port: '',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
